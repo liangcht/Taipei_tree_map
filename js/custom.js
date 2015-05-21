e266ba48209cea02d6d3f8d454f55c07d8800d96
tj474474 <tj474474@gmail.com>
2015-05-22 01:09:00 +0800

    Finished initialization's view

--- js/custom.js
+++ js/custom.js
@@ -29,13 +29,14 @@ radioArr = [],
 ibArr = [],
 inactiveMarker = "http://www.bestofbritishunsigned.com/music-map/images/band-marker.png",
 activeMarker = "http://www.bestofbritishunsigned.com/music-map/images/band-marker-selected.png",
-farZoom = 3,
+farZoom = 6,
 closeZoom = 12,
 maxZoomLevel = 3,
 closeLat = 25.043454,
 closeLng = 121.538808,
-farLat = 30.597528,
-farLng = - 9.69043;
+farLat = 25.043454,
+farLng = 121.538808,
+GoogleFusionTablesAPIkey = 'AIzaSyBCUUAjeEqzjKc3rIc7-pqFVFEpGFvsawo';
 
 
 
@@ -169,6 +170,7 @@ function updateFilters(markerArr) {
     genreList = genreList.clean("");
     showList = showList.clean("");
 }
+/******* Used by original method ***********
 function setData(tableid) {
     if (markerArr.length > 0) {
         markerArr = [];
@@ -201,6 +203,37 @@ function getData(response) {
             map.panTo(latLng);
         }
     }
+}
+*/
+function setDataAjax(tableid) {
+    var query = "SELECT * FROM " + tableid;
+    var encodedQuery = encodeURIComponent(query);
+
+    // Construct the URL
+    var url = ['https://www.googleapis.com/fusiontables/v2/query'];
+    url.push('?sql=' + encodedQuery);
+    url.push('&key=' + GoogleFusionTablesAPIkey);
+    url.push('&callback=?');
+
+    // Send the JSONP request using jQuery
+    $.ajax({
+        url: url.join(''),
+        dataType: 'jsonp',
+        success: function (data) {
+            var rows = data['rows'];
+            for (i = 0; i < rows.length; i++) {
+                dropMarkers(rows[i], i);
+            }
+            updateFilters(markerArr);
+            for (i = 0; i < markerArr.length; i++) {
+                var lat = markerArr[i][0].getPosition().lat();
+                var lng = markerArr[i][0].getPosition().lng();
+                map.setZoom(closeZoom);
+                var latLng = new google.maps.LatLng(closeLat, closeLng);
+                map.panTo(latLng);
+            }           
+        }
+    });
 }
 function timeAgo(dateString) {
     var rightNow = new Date();
@@ -347,9 +380,12 @@ function addMarker(coordinate, row) {
     return marker;
 }
 function dropMarkers(row, i) {
-    codeAddress(row, i);
+    if (row[21] && row[22]) {
+        codeAddress(row, i);
+    }
 }
 function codeAddress(row, currentIdx) {
+    /********** Original Version *************
     var tableCos = (type == 0) ? row[1]: row[2];
     tableCos = tableCos.replace(" ", "").split(",");
     var coordinate = new google.maps.LatLng(tableCos[0], tableCos[1]);
@@ -388,6 +424,106 @@ function codeAddress(row, currentIdx) {
                 dataHTML = dataHTML + '<hr /><div id="twitter-feed" data-account="' + twitAcc + '"><h4>Latest Tweet</h4><div id="loading-tweets"><img alt="Loading Data" title="Loading Data" src="images/loading.gif" width="32" height="32" /></div><div class="tweets"></div></div>';
             }
         }
+    } else {
+        if (type == 1) {
+            var dataHTML = "<h3>" + row[0] + '</h3><span class="station location">' + row[3] + "</span>";
+            if (row[4]) {
+                dataHTML = dataHTML + '<a class="webLink" href="' + row[4] + '" title="" target="_blank" rel="nofollow">' + row[4] + "</a>";
+            }
+            if (row[5] || row[6] || row[7]) {
+                dataHTML = dataHTML + '<ul class="social">';
+                if (row[7]) {
+                    dataHTML = dataHTML + '<li class="first facebook"><a href="' + row[7] + '" title="" target="_blank" rel="nofollow"></a></li>';
+                }
+                if (row[8]) {
+                    dataHTML = dataHTML + '<li class="twitter"><a href="' + row[8] + '" title="" target="_blank" rel="nofollow"></a></li>';
+                }
+                if (row[9]) {
+                    dataHTML = dataHTML + '<li class="last music"><a href="' + row[9] + '" title="" target="_blank" rel="nofollow"></a></li></ul>';
+                }
+            }
+            if (row[5]) {
+                dataHTML = dataHTML + '<span class="featuredOn">Broadcasting since ' + row[5] + "</span>";
+            }
+            if (row[6]) {
+                dataHTML = dataHTML + "<hr /><p>" + row[6].replace(/\n/g, "<br />") + "</p>";
+            }
+            if (row[8]) {
+                var twitAcc = row[8].split("/");
+                twitAcc = twitAcc[twitAcc.length - 1];
+                if (twitAcc) {
+                    dataHTML = dataHTML + '<hr /><div id="twitter-feed" data-account="' + twitAcc + '"><h4>Latest Tweet</h4><div id="loading-tweets"><img alt="Loading Data" title="Loading Data" src="images/loading.gif" width="32" height="32" /></div><div class="tweets"></div></div>';
+                }
+            }
+        }
+    }
+    google.maps.event.addListener(marker, "mouseover", function(event) {
+        createInfoBox(marker, row, currentIdx);
+    });
+    google.maps.event.addListener(marker, "mouseout", function(event) {
+        closeInfoBox(currentIdx);
+    });
+    google.maps.event.addListener(marker, "click", function(event) {
+        for (var i = 0, len = markerArr.length; i < len; i++) {
+            if (markerArr[i][0].clickable) {
+                markerArr[i][0].setIcon(inactiveMarker);
+            }
+            if (i != currentIdx) {
+                markerArr[i][1] = false;
+            }
+        }
+        changeData(marker, currentIdx, dataHTML);
+    });
+    google.maps.event.addListener(map, "zoom_changed", function() {
+        if (map.getZoom() < maxZoomLevel) {
+            map.setZoom(maxZoomLevel);
+        }
+    });
+    */
+
+    /******* My own version ***********/
+    //var tableCos = (type == 0) ? row[1]: row[2];
+    //tableCos = tableCos.replace(" ", "").split(",");
+    var coordinate = new google.maps.LatLng(row[22], row[21]);
+    var marker = addMarker(coordinate, row);
+    if (type == 0) {
+        var dataHTML = "<h3> No." + row[0].toString() + ',' + row[3] +'</h3><span class="location">' + row[18] + ',' + row[19] + "</span>";
+        /*
+        if (row[3]) {
+            dataHTML = dataHTML + '<a class="webLink" href="' + row[3] + '" title="" target="_blank" rel="nofollow">' + row[3] + "</a>";
+        }
+        if (row[5] || row[6] || row[7]) {
+            dataHTML = dataHTML + '<ul class="social">';
+            if (row[5]) {
+                dataHTML = dataHTML + '<li class="first facebook"><a href="' + row[5] + '" title="" target="_blank" rel="nofollow"></a></li>';
+            }
+            if (row[6]) {
+                dataHTML = dataHTML + '<li class="twitter"><a href="' + row[6] + '" title="" target="_blank" rel="nofollow"></a></li>';
+            }
+            if (row[7]) {
+                dataHTML = dataHTML + '<li class="last music"><a href="' + row[7] + '" title="" target="_blank" rel="nofollow"></a></li>';
+            }
+            dataHTML = dataHTML + "</ul>";
+        }
+        if (row[9]) {
+            dataHTML = dataHTML + '<span class="featuredOn">Featured on Show #' + row[9] + "</span>";
+        }
+        */
+        if (row[20]) {
+            dataHTML = dataHTML + "<hr /><p>" + autolink(row[20].replace(/\n/g, "<br />"), {
+                target: "_blank",
+                rel: "nofollow"
+            }) + "</p>";
+        }
+        /*
+        if (row[6]) {
+            var twitAcc = row[6].split("/");
+            twitAcc = twitAcc[twitAcc.length - 1];
+            if (twitAcc == "testing") {
+                dataHTML = dataHTML + '<hr /><div id="twitter-feed" data-account="' + twitAcc + '"><h4>Latest Tweet</h4><div id="loading-tweets"><img alt="Loading Data" title="Loading Data" src="images/loading.gif" width="32" height="32" /></div><div class="tweets"></div></div>';
+            }
+        }
+        */
     } else {
         if (type == 1) {
             var dataHTML = "<h3>" + row[0] + '</h3><span class="station location">' + row[3] + "</span>";
@@ -510,7 +646,7 @@ function initialize() {
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     map.mapTypes.set("map_style", styledMap);
     map.setMapTypeId("map_style");
-    setData(treeid);
+    setDataAjax(treeid);
 }
 $(window).on("load", this, function(e) {
     initialize();
@@ -531,6 +667,8 @@ jQuery(document).ready(function($) {
             }
         }
     });
+
+    /*
     $("#left-top a").on("click", this, function(e) {
         e.preventDefault();
         var thisID = $(this).attr("id").split("-")[0];
@@ -564,6 +702,7 @@ jQuery(document).ready(function($) {
             $autoComp.empty().slideUp(250);
         }
     });
+    */
     $searchIn.on("keyup", this, function(e) {
         var k = e.keyCode || e.which;
         var v = $(this).val().toLowerCase();
